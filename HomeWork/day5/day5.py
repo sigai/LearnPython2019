bffec7e82f7b7186c090d6d1f7763d441c656ecb
sigai <vipfts@qq.com>
2017-05-16 22:07:54 +0800

    test
    test

---
+++ HomeWork/day5/day5.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+__author__ = "Sigai"
+
+import re
+
+userinput = "1 - 2 * ( (60-30 +(-40/5) * (9-2*5/3 + 7 /3*99/4*2998 +10 * 568/14 )) - (-4*3)/ (16-3*2) )"
+
+def format(expression):
+    while " " in expression:
+        expression = expression.replace(" ","")
+    while "+-" in expression:
+        expression = expression.replace("+-","-")
+    while "--" in expression:
+        expression = expression.replace("--","+")
+    return expression
+
+def addition(expression):
+    pass
+
+def subtraction(expression):
+    pass
+
+def multiplication(expression):
+    pass
+
+def division(expression):
+    pass
+
+def brackets(expression):
+    pass
+
+def is_result(expression):
+    if "*" in expression or \
+        "/" in expression or \
+        "+" in expression or \
+        ("-" in expression and not expression.startswith("-")):
+        return False
+    else:
+        return True
+
+def
+expression = format(userinput)
+while not is_result(expression):
+    expression = brackets(expression)
---
+++ jandan/jandan-2.py
@@ -0,0 +1,67 @@
+canvas ="\033[1;37m█\033[0m"
+red = "\033[1;31m█\033[0m"
+yel = "\033[1;33m█\033[0m"
+blk = "\033[1;30m█\033[0m"
+gre = "\033[1;32m█\033[0m"
+mth = "\033[1;35m█\033[0m"
+#鸡冠▂　▃　▄　▅　▆　▇　█　▏　▎　▍　▌　▋　▊　▉
+print("".join([canvas*20,red*2,canvas*22]))
+print("".join([canvas*20,red*2,canvas*22]))
+print("".join([canvas*20,red*4,canvas*20]))
+print("".join([canvas*20,red*4,canvas*20]))
+#额头
+print("".join([canvas*16,yel*12,canvas*16]))
+print("".join([canvas*16,yel*12,canvas*16]))
+print("".join([canvas*12,yel*20,canvas*12]))
+print("".join([canvas*12,yel*20,canvas*12]))
+
+print("".join([canvas*10,yel*24,canvas*10]))
+print("".join([canvas*10,yel*24,canvas*10]))
+
+print("".join([canvas*8,yel*28,canvas*8]))
+print("".join([canvas*8,yel*28,canvas*8]))
+print("".join([canvas*8,yel*28,canvas*8]))
+print("".join([canvas*8,yel*28,canvas*8]))
+
+#眼睛
+print("".join([canvas*6,gre*2,yel*8,blk*2,yel*8,blk*2,yel*8,gre*2,canvas*6]))
+print("".join([canvas*6,gre*2,yel*8,blk*2,yel*8,blk*2,yel*8,gre*2,canvas*6]))
+print("".join([canvas*6,gre*2,yel*28,gre*2,canvas*6]))
+print("".join([canvas*6,gre*2,yel*28,gre*2,canvas*6]))
+#嘴巴
+print("".join([canvas*4,yel*16,mth*4,yel*16,canvas*4]))
+print("".join([canvas*4,yel*16,mth*4,yel*16,canvas*4]))
+print("".join([canvas*2,yel*18,mth*4,yel*18,canvas*2]))
+print("".join([canvas*2,yel*18,mth*4,yel*18,canvas*2]))
+#翅膀
+print("".join([canvas*0,yel*44]))
+print("".join([canvas*0,yel*44]))
+print("".join([canvas*0,yel*44]))
+print("".join([canvas*0,yel*44]))
+print("".join([canvas*0,yel*44]))
+print("".join([canvas*0,yel*44]))
+#腋下
+print("".join([canvas*0,yel*6,gre*2,yel*28,gre*2,yel*6]))
+print("".join([canvas*0,yel*6,gre*2,yel*28,gre*2,yel*6]))
+print("".join([canvas*4,gre*4,yel*28,gre*4,canvas*4]))
+print("".join([canvas*4,gre*4,yel*28,gre*4,canvas*4]))
+print("".join([canvas*6,gre*2,yel*28,gre*2,canvas*6]))
+print("".join([canvas*6,gre*2,yel*28,gre*2,canvas*6]))
+print("".join([canvas*6,gre*2,yel*28,gre*2,canvas*6]))
+print("".join([canvas*6,gre*2,yel*28,gre*2,canvas*6]))
+#肚子
+print("".join([canvas*8,gre*2,yel*24,gre*2,canvas*8]))
+print("".join([canvas*8,gre*2,yel*24,gre*2,canvas*8]))
+print("".join([canvas*10,gre*2,yel*20,gre*2,canvas*10]))
+print("".join([canvas*10,gre*2,yel*20,gre*2,canvas*10]))
+#鸡屁股
+print("".join([canvas*12,gre*2,blk*2,gre*12,blk*2,gre*2,canvas*12]))
+print("".join([canvas*12,gre*2,blk*2,gre*12,blk*2,gre*2,canvas*12]))
+#鸡腿
+print("".join([canvas*14,blk*2,canvas*12,blk*2,canvas*14]))
+print("".join([canvas*14,blk*2,canvas*12,blk*2,canvas*14]))
+#鸡爪
+print("".join([canvas*12,blk*6,canvas*8,blk*6,canvas*12]))
+print("".join([canvas*12,blk*6,canvas*8,blk*6,canvas*12]))
+#地面
+print("".join([canvas*44]))
