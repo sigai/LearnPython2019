b3396c41776e505af722326f94ea3e43abb74cd7
sigai <vipfts@qq.com>
2017-05-19 20:35:25 +0800

    test
    test

--- 14day6/4/cs_game.py
+++ 14day6/4/cs_game.py
@@ -4,22 +4,26 @@ __author__ = "Sigai"
 
 
 class Role(object):
-	def __init__(self, name, role, weapon, life_value=100, money=15000):
-		self.name = name
-		self.role = role
-		self.weapon = weapon
-		self.life_value = life_value
-		self.money = money
-	
-	def shot(self):
-		print("shooting...")
-	
-	def got_shot(self):
-		print("ah...,I got shot...")
-	
-	def buy_gun(self, gun_name):
-		print("just bought %s" % gun_name)
+    def __init__(self, name, role, weapon, life_value=100, money=15000):
+        self.name = name
+        self.role = role
+        self.weapon = weapon
+        self.life_value = life_value
+        self.money = money
+
+    def shot(self):
+        print("shooting...")
+
+    def got_shot(self):
+        print("%s:ah...,I got shot..." % self.name)
+
+    def buy_gun(self, gun_name):
+        print("just bought %s" % gun_name)
 
 
-r1 = Role('Alex', 'police', 'AK47') #生成一个角色
-r2 = Role('Jack', 'terrorist', 'B22')  #生成一个角色
+r1 = Role('Alex', 'police', 'AK47')  # 生成一个角色
+r2 = Role('Jack', 'terrorist', 'B22')  # 生成一个角色
+
+r1.buy_gun("b43")
+r1.got_shot()
+r2.got_shot()
--- 14day6/4/dog.py
+++ 14day6/4/dog.py
@@ -2,12 +2,15 @@
 # -*- coding: UTF-8 -*-
 __author__ = "Sigai"
 
+
 class Dog:
-	def __init__(self,name):
-		self.name = name
-	def bulk(self):
-		print("{name}:wang wang wang!".format(name = self.name))
-		
+    def __init__(self, name):
+        self.name = name
+
+    def bulk(self):
+        print("{name}:wang wang wang!".format(name=self.name))
+
+
 dog1 = Dog("No1")
 dog2 = Dog("No2")
 dog3 = Dog("No3")
