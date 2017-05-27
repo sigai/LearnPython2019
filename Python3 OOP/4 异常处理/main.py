752b2939aee73a6e1a16a66429bdbc2e95dd1eb9
sigai <vipfts@qq.com>
2017-05-27 12:30:16 +0800

    test
    test

---
+++ Python3 OOP/4 异常处理/main.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+__author__ = "Sigai"
+'''SystemExit KeyboardInterrupt需要用BaseException捕捉'''
+try:
+    raise SystemExit
+except BaseException as e:
+    print("Want to exit? error:%s"%e)
+finally:
+    print("Bye")
