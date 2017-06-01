174f42b3f4cd99b3dc8a240aaf4196751b1e9ffe
sigai <vipfts@qq.com>
2017-06-01 23:58:21 +0800

    test from pycharm

--- Python3 Advanced Tutorial/3 - Argparse/main.py
+++ Python3 Advanced Tutorial/3 - Argparse/main.py
@@ -2,3 +2,22 @@
 # -*- coding: UTF-8 -*-
 __author__ = "Sigai"
 
+import argparse
+
+def fib(n):
+    a, b = 0, 1
+    for i in range(n):
+        a, b = b, a+b
+    return a
+
+def Main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('num',help='The fib number you want to calc',type = int)
+
+    args = parser.parse_args()
+
+    result = fib(args.num)
+    print("The",args.num,'th fib number is',result)
+
+if __name__ == "__main__":
+    Main()
