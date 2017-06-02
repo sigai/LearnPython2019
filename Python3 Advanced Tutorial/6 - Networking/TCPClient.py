c1a8bf5a80017ba6ae9ffa82d104c5d5252d1694
sigai <vipfts@qq.com>
2017-06-02 18:08:29 +0800

    test from pycharm

---
+++ Python3 Advanced Tutorial/6 - Networking/TCPClient.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+__author__ = "Sigai"
+import socket
+
+def Main():
+    host = '127.0.0.1'
+    port = 5000
+
+    s = socket.socket()
+    s.connect((host,port))
+
+    message = input(">>>:")
+    while message != 'q':
+        s.send(message.encode())
+        data = s.recv(1024)
+        print("Recieved from server:",data.decode())
+        message = input(">>>:")
+    s.close()
+if __name__ == "__main__":
+    Main()
