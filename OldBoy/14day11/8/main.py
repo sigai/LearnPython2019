da3758acf034c5a88ac7b4de109f1322679fda5f
sigai <vipfts@qq.com>
2017-06-04 22:24:27 +0800

    test
    test

---
+++ OldBoy/14day11/8/main.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+__author__ = "Sigai"
+
+import redis
+
+pool = redis.ConnectionPool(host='127.0.0.1',port=6379)#连接池
+r = redis.Redis(connection_pool=pool)
+
+# r = redis.Redis(host='127.0.0.1',port=6379)
+r.set("foo","bar")
+print(r.get('foo'))
