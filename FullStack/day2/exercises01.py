3124be596d1f0798fd9d88be295f7198835d7a82
sigai <vipfts@qq.com>
2017-07-13 23:58:38 +0800

    1
    1

---
+++ FullStack/day2/exercises01.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+__author__ = "Sigai"
+
+numbers = [11,22,33,44,55,66,77,88,99,90]
+
+result = {'k1':[],'k2':[],}
+
+for i in numbers:
+    if i > 66:
+        result['k1'].append(i)
+    elif i < 66:
+        result['k2'].append(i)
+
+
+print(result)
