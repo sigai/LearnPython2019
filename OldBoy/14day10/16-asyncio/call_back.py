8e1241b9c0a4d6d1d0d6594e5972e9635187b55b
sigai <vipfts@qq.com>
2017-07-31 22:29:51 +0800

    1
    1

--- OldBoy/14day10/16-asyncio/call_back.py
+++ OldBoy/14day10/16-asyncio/call_back.py
@@ -3,6 +3,7 @@
 __author__ = "Sigai"
 import time
 import asyncio
+import functools
 
 now = lambda : time.time()
 
@@ -13,8 +14,11 @@ async def do_some_work(x):
 
 # future参数会在task结束之后接收实参task, 得到任务的结果result, 也就是协程的返回值.
 
-def callback(future):
-    print("callback: ", future.result())
+# def callback(future):
+#     print("callback: ", future.result())
+
+def callback(t, future):
+    print("callback: ", t, future.result())
 
 start = now()
 
@@ -23,7 +27,8 @@ loop = asyncio.get_event_loop()
 task = asyncio.ensure_future(coroutine)
 
 # 给task绑定回调函数, task结束后, 会把task对象作为参数传给回调函数.
-task.add_done_callback(callback)
+# task.add_done_callback(callback)
+task.add_done_callback(functools.partial(callback, 2))
 
 # task结束后, 也就是协程执行结束, 执行回调函数
 loop.run_until_complete(task)
