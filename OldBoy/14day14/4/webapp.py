59ca99a6076d47de127659b94bed11724013571c
sigai <vipfts@qq.com>
2017-08-14 18:24:34 +0800

    start day14
    HTML

---
+++ OldBoy/14day14/4/webapp.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+__author__ = "Sigai"
+import socket
+
+
+def handle_request(client):
+    buf = client.recv(1024)
+    client.send(bytes("HTTP/1.1 200 OK\r\n\r\n",encoding='utf-8'))
+    client.send(bytes("Hello, Cool Boy", encoding='utf-8'))
+
+
+def main():
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.bind(('localhost', 8000))
+    sock.listen(5)
+
+    while True:
+        connection, address = sock.accept()
+        handle_request(connection)
+        connection.close()
+
+
+if __name__ == '__main__':
+    main()
---
+++ OldBoy/14day14/note001.md
@@ -0,0 +1,11 @@
+#HTML和CSS
+
+HTML
+  - 20个标签
+
+CSS
+  - 颜色
+  - 位置
+
+所有的web应用本质上是socket服务端, 浏览器是socket客户端.
+HTTP连接是短连接
