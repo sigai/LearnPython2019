9742e6f28cafd63135c316b8c572b917db759402
sigai <vipfts@qq.com>
2018-02-28 11:59:16 +0800

    :sparkles: parse sitemap lastmod

--- try_things/try_rewrite_SitemapSpider/sitemap_parser.py
+++ try_things/try_rewrite_SitemapSpider/sitemap_parser.py
@@ -2,3 +2,17 @@
 # -*- coding: UTF-8 -*-
 __author__ = "Sigai"
 
+import requests
+
+from scrapy.utils.sitemap import Sitemap
+from scrapy.http import XmlResponse
+
+
+url = 'https://answers.microsoft.com/map/thread?day=1'
+
+body = requests.get(url)
+
+s = Sitemap(body.content)
+
+for d in s:
+    print(d)
