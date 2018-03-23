737bddcd2aa0c853b48b984363245591b591f70c
sigai <vipfts@qq.com>
2018-03-23 14:31:21 +0800

    :wrench: with sublime go

--- try_things/try_golang/gotut.go
+++ try_things/try_golang/gotut.go
@@ -1,11 +1,13 @@
 package main
 
-import ("fmt";"math/rand")
+import (
+	"fmt"
+	"math/rand"
+)
 
 func foo() {
 	fmt.Println("A random int number:", rand.Intn(100))
 }
-
 
 func main() {
 	fmt.Println("fuck world!")
