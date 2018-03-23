eaaf4a7bf4d1cedb6c8943ad051a37fd07d8b241
sigai <vipfts@qq.com>
2018-03-23 21:50:37 +0800

    :wrench:  fix the gitattributes

--- try_things/try_golang/gotut.go
+++ try_things/try_golang/gotut.go
@@ -2,14 +2,9 @@ package main
 
 import (
 	"fmt"
-	"math/rand"
 )
 
-func foo() {
-	fmt.Println("A random int number:", rand.Intn(100))
-}
-
 func main() {
-	fmt.Println("fuck world!")
-	foo()
+	var a int16
+	fmt.Println()
 }
