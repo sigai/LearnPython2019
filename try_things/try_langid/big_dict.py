87561066e9ec99d641cebbe5fbf6f5cfa875e3ce
sigai <vipfts@qq.com>
2018-04-07 00:55:45 +0800

    :disappointed:  not work well

---
+++ try_things/try_langid/big_dict.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+__author__ = "Sigai"
+
+headers = {"Accept": "*/*",
+           "Accept-Encoding": "gzip, deflate, br",
+           "Accept-Language": "en-US,en;q=0.9,zh-CN;q=0.8,zh;q=0.7",
+           "Cache-Control": "no-cache",
+           "Connection": "keep-alive",
+           "DNT": "1",
+           "Host": "fanyi.baidu.com",
+           "Pragma": "no-cache",
+           "Referer": "https://fanyi.baidu.com/",
+           "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_4) AppleWebKit/537.36 (KHTML, like Gecko) "
+                         "Chrome/67.0.3381.0 Safari/537.36",
+           "X-Requested-With": "XMLHttpRequest",
+           }
