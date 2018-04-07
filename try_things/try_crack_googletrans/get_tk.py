d746e0d80ceef60319dfa1adb1321adaa7524797
sigai <vipfts@qq.com>
2018-04-07 12:48:00 +0800

    :disappointed:  failed get tk

---
+++ try_things/try_crack_googletrans/get_tk.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+__author__ = "Sigai"
+
+# import requests
+# url = "https://translate.google.com/"
+# headers = {"user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3386.1 Safari/537.36"}
+# res = requests.get(url)
+# print(res)
+
+
+TKK = f"423028.{3838261161 + -3064700435}"
+print(TKK)
+
+from googletrans.gtoken import TokenAcquirer
