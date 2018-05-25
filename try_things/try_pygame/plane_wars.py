e302e56a20142a36d1ed02d08792b93a77d618ca
sigai <vipfts@qq.com>
2018-05-25 14:02:01 +0800

    :wrench:  play plane

--- try_things/try_pygame/plane_wars.py
+++ try_things/try_pygame/plane_wars.py
@@ -9,15 +9,18 @@ from pygame.locals import *
 def main():
     pygame.init()
     pygame.display.set_caption("Plane Wars III")
-    screen = pygame.display.set_mode((480, 852),0,32)
     background = pygame.image.load("data/image/background.png")
+    hero = pygame.image.load("data/image/hero1.png")
+    screen = pygame.display.set_mode((background.get_width(), background.get_height()), 0, 32)
 
     while True:
         for event in pygame.event.get():
             if event.type == QUIT:
                 return
         screen.blit(background, (0,0))
+        screen.blit(hero, ((background.get_width() - hero.get_width())//2, background.get_height() - hero.get_height()))
         pygame.display.update()
+
 
 
 if __name__ == '__main__':
